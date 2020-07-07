a173dd4e90a060a1511103ba690090128bd25874
Vitor Lescura <vitorlescura@gmail.com>
2020-07-07 00:38:41 -0300

    Classes Modelo

--- src/br/com/modelo/Cliente.java
+++ src/br/com/modelo/Cliente.java
@@ -1,5 +1,22 @@
 package br.com.modelo;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cliente extends Pessoa{
+	public List<Pet> listaPets = new ArrayList<Pet>();
+	
+	public void setPet(Pet pet) {
+		listaPets.add(pet);
+	}
+	
+	public List getNomePet() {
+		return listaPets;
+	}
+	
+	public void imprimePets() {
+		for(Pet pet: listaPets) {
+			pet.imprimePet();
+		}
+	}
 }
--- src/br/com/modelo/DataHora.java
+++ src/br/com/modelo/DataHora.java
@@ -1,5 +1,29 @@
 package br.com.modelo;
 
 public class DataHora {
+	private String anterior;
+	private String atual;
+			
+	public DataHora(String anterior, String atual) {
+		this.anterior = anterior;
+		this.atual = atual;
+	}
 
-}
+	public String getDataAnterior() {
+		return anterior;
+	}
+
+	public void setDataAnterior(String anterior) {
+		this.anterior = anterior;
+	}
+
+	public String getDataAtual() {
+		return atual;
+	}
+
+	public void setDataAtual(String atual) {
+		this.atual = atual;
+	}
+	
+	
+} // FIM
